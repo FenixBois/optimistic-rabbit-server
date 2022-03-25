c0a734e8c32f393bea4cc55311fb58483120630c
Adam Tretera <adam.tretera@ackee.cz>
2022-03-25 18:49:40 +0100

    🎨 structure

--- src/api/controllers/recepie.controller.ts
+++ src/api/controllers/recepie.controller.ts
@@ -1,12 +1,36 @@
-import { getRecepies } from '../services/recepie.service';
+import { getRecepies, getRecepie ,createRecepie} from '../services/recepie.service';
 import { NextFunction, Request, Response, Router } from 'express';
 
 const router = Router();
 
 router.get('/recepies', async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const result = await getRecepies(req.query);
+        const result = await getRecepies();
         res.json(result);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.get('/recepie/:slug', async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        
+        const article = await getRecepie(req.params.slug);
+        res.json({ article });
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.post('/recepie', async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const article = await createRecepie({
+            title: req.body.title,
+            content: req.body.description,
+            slug: req.body.title,
+            ingredients: req.body.ingredients,
+        });
+        res.json({ article });
     } catch (error) {
         next(error);
     }
--- src/api/services/recepie.service.ts
+++ src/api/services/recepie.service.ts
@@ -1,6 +1,8 @@
 import prisma from '../../config/prisma';
+import { Prisma } from '@prisma/client';
+import slugify from 'slugify';
 
-export const getRecepies = async (query: any) => {
+export const getRecepies = async () => {
     const recepies = await prisma.recepie.findMany({
         orderBy: {
             createdAt: 'desc',
@@ -11,5 +13,56 @@ export const getRecepies = async (query: any) => {
 
     return {
         recepies,
+    };
+};
+
+export const getRecepie = async (slug: string) => {
+    const recepie = await prisma.recepie.findUnique({
+        where: {
+            slug: slug,
+        },
+    });
+
+    return {
+        recepie,
+    };
+};
+
+export const createRecepie = async (recepie: Prisma.RecepieCreateInput) => {
+    let slug = slugify(recepie.title);
+
+    const existingTitle = await prisma.recepie.findUnique({
+        where: {
+            slug,
+        },
+        select: {
+            slug: true,
+        },
+    });
+    if (existingTitle) {
+        // TODO: replace with error handler / express validator
+        throw new Error('Must be uniq');
+    }
+    if (!recepie.ingredients) {
+        // TODO: replace with error handler / express validator
+        throw new Error('Must be uniq');
+    }
+
+    // @ts-ignore
+    const newRecepie = await prisma.recepie.create({
+        data: {
+            ...recepie,
+            slug,
+            ingredients: {
+                // @ts-ignore
+                create: recepie.ingredients.map((ingredient: string) => ({
+                    name: ingredient 
+                })),
+            },
+        },
+    });
+
+    return {
+        newRecepie,
     };
 };
