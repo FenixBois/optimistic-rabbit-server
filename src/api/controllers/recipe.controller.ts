359d2cf0b509fb035dfb4d3f4aaa6cf55de9d650
kammet07 <pavel.pupentsov@gmail.com>
2022-05-19 09:39:27 +0200

    wip7

--- src/api/controllers/recipe.controller.ts
+++ src/api/controllers/recipe.controller.ts
@@ -1,11 +1,10 @@
 import { NextFunction, Request, RequestHandler, Response, Router } from 'express';
 import { createRecipe, getAllRecipes, getRecipe } from '../services/recipe.service';
-import logger, { requestLogger } from '../../config/winston';
+import { requestLogger } from '../../config/winston';
 import { body, check, matchedData, param, query, ValidationChain, validationResult } from 'express-validator';
 import { ValidationException } from '../../errors/ValidationException';
 import prisma from '../../config/prisma';
 import { difficulty, media, taste, unit } from '../../config/models';
-import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 const router = Router();
 
@@ -14,9 +13,9 @@ const FilterValidation: ValidationChain[] = [
     query('servings').if(query('servings').exists()).isNumeric().toInt(),
     query('time').if(query('time').exists()).isNumeric().toInt(),
     query('reference').if(query('reference').exists()).isString(),
-    query('difficulty').if(query('difficulty').exists()).isIn(Object.keys(difficulty)),
-    query('media').if(query('media').exists()).isIn(Object.keys(media)),
-    query('taste').if(query('taste').exists()).isIn(Object.keys(taste)),
+    query('difficulty').if(query('difficulty').exists()).toLowerCase().isIn(Object.keys(difficulty)),
+    query('media').if(query('media').exists()).toLowerCase().isIn(Object.keys(media)),
+    query('taste').if(query('taste').exists()).toLowerCase().isIn(Object.keys(taste)),
     query('vegetarian').if(query('vegetarian').exists()).isBoolean().toBoolean(),
 ];
 
@@ -32,13 +31,13 @@ const throwIfInvalid: RequestHandler = (req: Request, res: Response, next: NextF
 //get all/filtered recipes
 router.get('/recipes', ...FilterValidation, throwIfInvalid, async (req: Request, res: Response) => {
     requestLogger(req);
-    try {
-        const result = await getAllRecipes(matchedData(req));
-        res.json(result);
-    } catch (err) {
-        logger.error(err);
-        return res.status(500).send('Something went wrong');
-    }
+    // try {
+    const result = await getAllRecipes(matchedData(req));
+    res.json(result);
+    // } catch (err) {
+    //     logger.error(err);
+    //     return res.status(500).send('Something went wrong');
+    // }
 });
 
 //get recipe detail
@@ -48,22 +47,22 @@ router.get(
     throwIfInvalid,
     async (req: Request, res: Response) => {
         requestLogger(req);
-        try {
-            const result = await getRecipe(matchedData(req).id);
-            if (!result) {
-                return res.status(404).send('Entity not found');
-            }
-
-            res.json(result);
-        } catch (err) {
-            logger.error(err);
-            if (err instanceof PrismaClientKnownRequestError) {
-                console.log('hello');
-                if (err.code === 'P2025') return res.status(404).send('Record does not exist.');
-                return res.status(500).send('Something went wrong');
-            }
-            return res.status(500).send('Something went wrong');
+        // try {
+        const result = await getRecipe(matchedData(req).id);
+        if (!result) {
+            return res.status(404).send('Entity not found');
         }
+
+        res.json(result);
+        // } catch (err) {
+        //     logger.error(err);
+        //     if (err instanceof PrismaClientKnownRequestError) {
+        //         console.log('hello');
+        //         if (err.code === 'P2025') return res.status(404).send('Record does not exist.');
+        //         return res.status(500).send('Something went wrong');
+        //     }
+        //     return res.status(500).send('Something went wrong');
+        // }
     },
 );
 
@@ -73,25 +72,25 @@ const RecipeValidation: ValidationChain[] = [
     body('servings').isNumeric(),
     body('time').isNumeric(),
     body('reference').isString(),
-    body('difficulty').isIn(Object.keys(difficulty)),
-    body('media').isIn(Object.keys(media)),
-    body('taste').isIn(Object.keys(taste)),
+    body('difficulty').toLowerCase().isIn(Object.keys(difficulty)),
+    body('media').toLowerCase().isIn(Object.keys(media)),
+    body('taste').toLowerCase().isIn(Object.keys(taste)),
     body('vegetarian').isBoolean(),
 
     check('ingredients.*.name').isString(),
     check('ingredients.*.amount').isNumeric(),
-    check('ingredients.*.unit').isIn(Object.keys(unit)),
+    check('ingredients.*.unit').toLowerCase().isIn(Object.keys(unit)),
 ];
 
 router.post('/recipe', ...RecipeValidation, throwIfInvalid, async (req: Request, res: Response) => {
     requestLogger(req);
-    try {
-        const result = await createRecipe(req.body);
-        res.json(result);
-    } catch (err) {
-        logger.error(err);
-        return res.status(500).send('Something went wrong');
-    }
+    // try {
+    const result = await createRecipe(req.body);
+    res.json(result);
+    // } catch (err) {
+    //     logger.error(err);
+    //     return res.status(500).send('Something went wrong');
+    // }
 });
 
 //delete recipe
@@ -101,17 +100,17 @@ router.delete(
     throwIfInvalid,
     async (req: Request, res: Response) => {
         requestLogger(req);
-        try {
-            await prisma.recipes.delete({ where: { id: matchedData(req).id } });
-            res.send('Success');
-        } catch (err) {
-            logger.error(err);
-            if (err instanceof PrismaClientKnownRequestError) {
-                if (err.code === 'P2025') return res.status(404).send('Record to delete does not exist.');
-                return res.status(500).send('Something went wrong');
-            }
-            return res.status(500).send('Something went wrong');
-        }
+        // try {
+        await prisma.recipes.delete({ where: { id: matchedData(req).id } });
+        res.send('Success');
+        // } catch (err) {
+        //     logger.error(err);
+        //     if (err instanceof PrismaClientKnownRequestError) {
+        //         if (err.code === 'P2025') return res.status(404).send('Record to delete does not exist.');
+        //         return res.status(500).send('Something went wrong');
+        //     }
+        //     return res.status(500).send('Something went wrong');
+        // }
     },
 );
 
--- src/errors/ErrorHandler.ts
+++ src/errors/ErrorHandler.ts
@@ -2,6 +2,7 @@ import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
 import { EntityNotFound } from './EntityNotFound';
 import { ValidationException } from './ValidationException';
 import logger from '../config/winston';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 export const ErrorHandler: ErrorRequestHandler = (err, req: Request, res: Response, _: NextFunction) => {
     logger.error({
@@ -11,17 +12,16 @@ export const ErrorHandler: ErrorRequestHandler = (err, req: Request, res: Respon
         body: req.body,
     });
 
-    if (err instanceof EntityNotFound) {
-        return res.status(404).json(err.message);
+    if (err instanceof EntityNotFound) return res.status(404).json(err.message);
+
+    if (err instanceof ValidationException) return res.status(400).json(err.errors.array());
+
+    if (err instanceof PrismaClientKnownRequestError) {
+        if (err.code === 'P2025') return res.status(404).send('Record does not exist.');
+        return res.status(500).send('Something went wrong');
     }
 
-    if (err instanceof ValidationException) {
-        return res.status(400).json(err.errors.array());
-    }
-
-    if (err instanceof Error) {
-        return res.status(500).json('Something went wrong');
-    }
+    if (err instanceof Error) return res.status(500).json('Something went wrong');
 
     return res.status(500).json('Something went wrong');
 };
